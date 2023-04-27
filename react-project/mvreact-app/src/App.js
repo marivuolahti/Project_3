401fa2f7af2ba99f947362ca17c93ec4c872691c
marivuolahti <78043421+marivuolahti@users.noreply.github.com>
2023-04-27 19:55:58 +0300

    Start
    Starting the project

---
+++ react-project/mvreact-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+
+const App = () => {
+const now = new Date()
+const a = 10
+const b = 20
+return (
+<div>
+<p>Hello world, it is {now.toDateString()}</p>
+<p>
+{a} plus {b} is {a + b}
+</p>
+</div>
+)
+}
+export default App
